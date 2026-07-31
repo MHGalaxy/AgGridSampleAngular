4a7937c681bd4578f483fc453093ea06d310e623
Mohammad Hosein Kahkeshan <mo.ho.kahkeshan@gmail.com>
2026-08-01 00:40:29 +0330

    config the themes in project

--- src/app/themes/ag-grid-theme.ts
+++ src/app/themes/ag-grid-theme.ts
@@ -1,32 +1,29 @@
 import { themeQuartz } from 'ag-grid-community';
 
 export const myTheme = themeQuartz.withParams({
-  accentColor: '#68FF8E',
-  backgroundColor: '#1f2836',
-  borderColor: '#429356',
-  borderRadius: 0,
-  browserColorScheme: 'dark',
-  cellHorizontalPaddingScale: 0.8,
-  cellTextColor: '#50F178',
+  accentColor: "#4E06B9",
+  backgroundColor: "#FFFFFF",
+  borderColor: "#D7E2E6",
+  borderRadius: 2,
+  browserColorScheme: "light",
+  cellHorizontalPaddingScale: 0.7,
   chromeBackgroundColor: {
-    ref: 'foregroundColor',
-    mix: 0.07,
-    onto: 'backgroundColor',
+    ref: "backgroundColor"
   },
-  columnBorder: true,
+  columnBorder: false,
   fontFamily: {
-    googleFont: 'IBM Plex Mono',
+    googleFont: "Inter"
   },
-  fontSize: 12,
-  foregroundColor: '#FFF',
-  headerBackgroundColor: '#21222C',
-  headerFontWeight: 700,
-  headerTextColor: '#68FF8E',
-  headerVerticalPaddingScale: 1.5,
-  oddRowBackgroundColor: '#21222C',
+  fontSize: 13,
+  foregroundColor: "#555B62",
+  headerBackgroundColor: "#FFFFFF",
+  headerFontSize: 13,
+  headerFontWeight: 400,
+  headerTextColor: "#84868B",
   rowBorder: true,
-  rowVerticalPaddingScale: 1.5,
-  spacing: 4,
-  wrapperBorder: true,
-  wrapperBorderRadius: 0,
+  rowVerticalPaddingScale: 0.8,
+  sidePanelBorder: true,
+  spacing: 6,
+  wrapperBorder: false,
+  wrapperBorderRadius: 2
 });
